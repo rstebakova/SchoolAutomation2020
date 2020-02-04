219ac8e3752317e28b09e1fbffe783225a2234da
Rita Stebakova <Rita Stebakova@rcomext.com>
2020-02-04 15:03:41 +0200

    Black box testing

--- lesson5/src/test/com/ctco/testSchool/BlackBoxTesting.java
+++ lesson5/src/test/com/ctco/testSchool/BlackBoxTesting.java
@@ -1,5 +1,10 @@
 package com.ctco.testSchool;
 
+import static org.hamcrest.core.Is.is;
+
+import java.util.Arrays;
+
+import com.ctco.testSchool.Member.type;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,23 +12,166 @@ public class BlackBoxTesting {
 
     Team myTeam = new Team();
 
+    Member first_qa = new Member(type.TEST);
+
+    Member second_qa = new Member(type.TEST);
+
+    Member first_dev = new Member(type.DEV);
+
+    Member second_dev = new Member(type.DEV);
+
+
+    /*Happy Path: team can deliver stories. 1 tester and  1 first_dev work 1 story; */
     @Test
-    public void emptyTeamTest() {
-        Assert.assertEquals("Empty team has zero velicity", 0.0, myTeam.getTeamVelocity(), 0.1);
+    public void happyCanDeliverQualityTestOneTestersOneDeveloper() {
+        // add team members
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(1);
+        myTeam.addMember(first_dev);
+
+        // define stories per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(2);
+
+        myTeam.backlog = Arrays.asList(first_story);
+        Assert.assertThat("Can be delivered.", myTeam.canDeliverQuality(), is(true));
     }
 
+    /* - Happy Path: team can deliver stories. 2 tester and  2 first_dev work */
     @Test
-    public void happyPathTest() {
-        Member member = new Member();
-        myTeam.addMember(member);
-        Assert.assertEquals("By default my velocity is 1 per 10 days of sprint", 10.0, myTeam.getTeamVelocity(), 0.1);
+    public void happyCanDeliverQualityTestAllTeam() {
+
+
+        // add qa team members
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(1);
+
+        myTeam.addMember(first_qa);
+        second_qa.setVelocity(1);
+
+        // add dev team members
+        myTeam.addMember(first_dev);
+        myTeam.addMember(second_dev);
+
+        // define stories per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(2);
+
+        Story second_story = new Story();
+        second_story.setStoryPoints(2);
+        second_story.setTestPoints(3);
+
+        Story third_story = new Story();
+        third_story.setStoryPoints(5);
+        third_story.setTestPoints(5);
+
+        myTeam.backlog = Arrays.asList(first_story,second_story);
+        Assert.assertThat("Can be delivered.", myTeam.canDeliverQuality(), is(true));
     }
 
+    /*Negative case: team can not deliver stories. with one tester in team and 2 devs work in parallel;*/
     @Test
-    public void customSprintLength() {
-        Member member = new Member();
-        myTeam.addMember(member);
-        myTeam.sprintDays = 9;
-        Assert.assertEquals("Sprint is 9 days long", 9.0, myTeam.getTeamVelocity(), 0.1);
+    public void negativeCanDeliverQualityTestTwoDevelopersOneTester() {
+
+        // add qa team members
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(0.5);
+
+        // add dev team members
+        myTeam.addMember(first_dev);
+        myTeam.addMember(second_dev);
+
+        // define stories per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(2);
+
+        Story second_story = new Story();
+        second_story.setStoryPoints(2);
+        second_story.setTestPoints(3);
+
+        Story third_story = new Story();
+        third_story.setStoryPoints(5);
+        third_story.setTestPoints(5);
+
+        myTeam.backlog = Arrays.asList(first_story,third_story);
+        Assert.assertThat("Should be False. Can't be delivered. Tester won't have enaugh time ", myTeam.canDeliverQuality(), is(false));
     }
+
+  /* - Negative case: team can not deliver stories. with two tester in team and 1 devs work if sprint is short;*/
+    @Test
+    public void negativeCanDeliverQualityShortSprint() {
+        myTeam.sprintDays = 5;
+
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(1);
+        myTeam.addMember(second_qa);
+        second_qa.setVelocity(1.5);
+
+        myTeam.addMember(first_dev);
+
+        // define stories per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(3);
+
+        myTeam.backlog = Arrays.asList(first_story);
+        Assert.assertThat("Can't be delivered. Sprint is too short", myTeam.canDeliverQuality(), is(false));
+    }
+
+    /* - Negative Path: team can not deliver stories if testers velocity = 0*/
+    @Test
+    public void negativeCanDeliverQualityWithZeroVelocityForQA() {
+        // add sprint days
+        myTeam.sprintDays = 5;
+
+        // add qa member with velocity 0
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(0);
+
+        // add first_dev member with velocity 0.5
+        myTeam.addMember(first_dev);
+        first_dev.setVelocity(0.5);
+
+        // define story per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(3);
+
+        // add story to backlog
+        myTeam.backlog = Arrays.asList(first_story);
+
+        //verify that scope can't be delivered
+        Assert.assertThat("Can't be delivered if tester velocity is null", myTeam.canDeliverQuality(), is(false));
+    }
+
+    /*Negative Path: team can not deliver stories if first_dev's velocity = 0 */
+    @Test
+    public void negativeCanDeliverQualityWithZeroVelocityForDEV() {
+
+        // add sprint days
+        myTeam.sprintDays = 5;
+
+        // add qa member with velocity 1
+        myTeam.addMember(first_qa);
+        first_qa.setVelocity(1);
+
+        // add first_dev member with velocity 0
+        myTeam.addMember(first_dev);
+        first_dev.setVelocity(0);
+
+        // define story per scope
+        Story first_story = new Story();
+        first_story.setStoryPoints(3);
+        first_story.setTestPoints(3);
+
+        // add story to backlog
+        myTeam.backlog = Arrays.asList(first_story);
+
+        //verify that scope can't be delivered
+        Assert.assertThat("Can't be delivered if first_dev's velocity is null", myTeam.canDeliverQuality(), is(false));
+    }
+
 }
